32c528d680bd7db4e63c2cfa8bb296bcd3ca9bfa
hjung0920 <likekhj@sookmyung.ac.kr>
2026-09-17 15:54:55 +0900

    03

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b;
+    float result;
+
+    printf("enter the numerator :");
+    scanf("%d", &a);
+
+    printf("enter the denominator :");
+    scanf("%d", &b);
+    
+    result = (float)a / b;
+    printf("the result of division if %f\n", result);
+
+    return 0;
+}
